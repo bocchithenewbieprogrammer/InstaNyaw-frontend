1d45386db4e51a0b1d9b33f72f549445c91c3ed2
bocchithenewbieprogrammer <medinabjorn@gmail.com>
2025-12-17 08:07:59 +0800

    Delete backend directory

--- backend/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { addComment, getComments } = require("../controllers/commentController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.post("/", protect, addComment);
-router.get("/", protect, getComments);
-
-module.exports = router;
